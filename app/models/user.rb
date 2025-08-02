30ccf3844e197abc2791c3e15d8983cd5d0dd9df
Hiromichi-Akiyama <fox37450@gmail.com>
2025-08-02 10:37:08 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 
   has_many :prototypes
   has_many :comments
